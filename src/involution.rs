e691c2c7ca76c042d0493ecf5ad6e11811009e4f
L Caraco <lbc.dna@gmail.com>
2021-04-06 16:55:12 -0500

    added basic reduce

--- src/involution.rs
+++ src/involution.rs
@@ -1,13 +1,35 @@
-use std::{collections::VecDeque, fmt::Display};
+use std::{collections::VecDeque, fmt::Display, cmp::*};
 
 use crate::traits::{Identity, Mapping};
 
-#[derive(Debug, PartialEq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub struct Involution(u8, u8);
+
+pub type Swap = Involution;
 
 impl Involution {
     pub fn new(a: u8, b: u8) -> Self {
         Involution(a.min(b), a.max(b))
+    }
+
+    pub fn contains(&self, x: u8) -> bool {
+        self.0 == x || self.1 == x
+    }
+
+    pub fn overlaps(&self, other: &Self) -> bool {
+        other.contains(self.0) || other.contains(self.1)
+    }
+
+    pub fn into_tuple(&self) -> (u8, u8) {
+        (self.0, self.1)
+    }
+
+    pub fn low(&self) -> u8 {
+        self.0
+    }
+
+    pub fn high(&self) -> u8 {
+        self.1
     }
 }
 
@@ -38,11 +60,11 @@ pub trait FromInvolutions {
 }
 
 #[derive(Debug, PartialEq, Clone)]
-pub struct InvlSeq(VecDeque<Involution>);
+pub struct SwapSeq(VecDeque<Involution>);
 
-impl InvlSeq {
+impl SwapSeq {
     pub fn new() -> Self {
-        InvlSeq(VecDeque::with_capacity(4))
+        Self(VecDeque::with_capacity(4))
     }
 
     pub fn compose_left(&mut self, other: Involution) {
@@ -52,9 +74,44 @@ impl InvlSeq {
     pub fn compose_right(&mut self, other: Involution) {
         self.0.push_back(other);
     }
+
+    pub fn reduce(&mut self) {
+        let mut out: VecDeque<Swap> = VecDeque::with_capacity(self.0.len());
+        for curr in self.0.iter().rev() {
+            if out.is_empty() {
+                out.push_back(*curr);
+                continue;
+            }
+            let mut i = 0;
+            loop {
+                let item = out[i];
+                
+                // if found identical cycle, remove both
+                if curr == &item {
+                    out.remove(i);
+                    break;
+                }
+                // if found non-traversable cycle, stop here and insert
+                if curr.overlaps(&item) {
+                    out.insert(i, *curr);
+                    break;
+                }
+                
+                // otherwise advance past the item
+                i += 1;
+                
+                // if no matches in out, just push to the back
+                if i == out.len() {
+                    out.push_back(*curr);
+                    break;
+                }
+            }
+        }
+        self.0 = out;
+    }
 }
 
-impl Mapping for InvlSeq {
+impl Mapping for SwapSeq {
     fn apply(&self, v: u8) -> u8 {
         self.0.iter().rev().fold(v, |x, inv| inv.apply(x))
     }
@@ -64,9 +121,9 @@ impl Mapping for InvlSeq {
     }
 }
 
-impl Identity for InvlSeq {
+impl Identity for SwapSeq {
     fn identity(_ord: u8) -> Self {
-        InvlSeq::new()
+        Self::new()
     }
 
     fn is_identity(&self) -> bool {
