ceb1646b947df877b53c94cb980fe95eff1a43de
L Caraco <lbc.dna@gmail.com>
2021-04-06 12:36:15 -0500

    made more perm64

--- src/perm64.rs
+++ src/perm64.rs
@@ -1,13 +1,14 @@
-use crate::traits::Mapping;
+use crate::{involution::{FromInvolutions, Involution}, traits::{Composable, Identity, Mapping}};
 
 /// OneLine representation for < S16
 /// Each nibble is an entry ie:
 /// `0xfedcba9876543210` is identity, where
 /// mapping for x is at (x-1)th nibble + 1.
 ///
-/// Involution(2, 4) is
+/// eg Involution(2, 4) is
 /// `0xfe...541230`
-pub struct Perm64(pub u64);
+#[derive(Debug, PartialEq, PartialOrd, Clone, Copy)]
+pub struct Perm64(u64);
 
 impl Mapping for Perm64 {
     fn apply(&self, v: u8) -> u8 {
@@ -19,6 +20,36 @@ impl Mapping for Perm64 {
     }
 
     fn order(&self) -> u8 {
-        todo!()
+        16
+    }
+}
+
+impl Composable<Perm64> for Perm64 {
+    fn compose(&self, right: &Self) -> Perm64 {
+        let mut data = 0;
+        for i in 1..=16 {
+            data |= (self.apply(right.apply(i)) as u64 - 1) << (4 * (i - 1));
+        }
+        Perm64(data)
+    }
+}
+
+impl FromInvolutions for Perm64 {
+    fn from_involutions(left: &Involution, right: &Involution) -> Self {
+        let mut data = 0;
+        for i in 1..=16 {
+            data |= (left.apply(right.apply(i)) as u64 - 1) << (4 * (i - 1));
+        }
+        Perm64(data)
+    }
+}
+
+impl Identity for Perm64 {
+    fn identity(_ord: u8) -> Self {
+        Perm64(0xFEDCBA9876543210)
+    }
+
+    fn is_identity(&self) -> bool {
+        self.0 == 0xFEDCBA9876543210
     }
 }
